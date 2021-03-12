6771453f9e4e0bd7ece6bad4a5113dc174d36713
Michael Jones <mtjones1979@yahoo.com>
2021-03-11 19:36:05 -0600

    got to cycle through iquirer quesitons.

--- index.js
+++ index.js
@@ -1,112 +1,153 @@
 const inquirer = require("inquirer");
-
+const fs = require("fs");
+const path = require("path");
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const generatedHTML = require("./template.js");
-const fs = require("fs");
-const { writeFileSync } = require("jest-serializer");
 
+let finalTeam = [];
 
-// function createTeam() {
-
-    const managerQuestions = () => {
-        console.log("Hello Manager!! Please build your team");
-        return inquirer.prompt ([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is your name?',
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: 'Please enter your ID number:',
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Please enter your email:',
-            },
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: 'Please enter your Office Number:',
-            },
-        ])
-        // . then(responses => {
-        //   const manager = new Manager(responses.name, responses.id, responses.email, reponses.officeNumber);
-        //  })
-    }
-    // const employeeChoice = () => {
-    //     return inquirer.prompt ([
-    //         {
-    //             type: 'list',
-    //             name: 'employee',
-    //             message: 'Which type of Team Member do you want to add?',
-    //             choices: [
-    //                 "Engineer",
-    //                 "Intern",
-    //                 "I don't want to add anymore Team Members"
-    //             ]
-    //         }
-    //     ])
-    // }
-    // const engineerQuestions = () => {
-        
-    //     return inquirer.prompt ([
-    //         {
-    //             type: 'input',
-    //             name: 'name',
-    //             message: "What is your Engineer's name?",
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'id',
-    //             message: 'Please enter their ID number:',
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'email',
-    //             message: 'Please enter their email:',
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'github',
-    //             message: 'Please enter their GitHub username:',
-    //         },
-    //     ])
-    // }
-    // const internQuestions = () => {
-        
-    //     return inquirer.prompt ([
-    //         {
-    //             type: 'input',
-    //             name: 'name',
-    //             message: "What is your Intern's name?",
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'id',
-    //             message: 'Please enter their ID number:',
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'email',
-    //             message: 'Please enter their email:',
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'school',
-    //             message: 'Please enter their School name:',
-    //         },
-    //     ])
-    // }
-// }
-const init = () => {
-    managerQuestions()
-    .then((responses => writeFileSync('index.html', generatedHTML(responses))))
-    .then(() => console.log('Successfully inputted your team!'))
-    .catch((err) => console.log(err))
+function startingPrompt () {
+    inquirer.prompt([
+        {
+            message: "Managers: Welcome to your Team Generator, to start building your team please pick a Team Name:",
+            name: "teamname"
+        }
+    ])
+    .then(function(data){
+        const teamName = data.teamname
+        finalTeam.push(teamName)
+        addManager();
+    })
 }
-init();
+function addManager () {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID number:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email:',
+        },
+        {
+            type: 'number',
+            name: 'officeNumber',
+            message: 'Please enter your Office Number:',
+        },
+    ])
+        .then(function (data) {
+            const name = data.name
+            const id = data.id
+            const email = data.email
+            const officeNumber = data.officeNumber
+            const teamMember = new Manager(name, id, email, officeNumber)
+            finalTeam.push(teamMember)
+            addTeamMember();
+        })
+}
+  function addTeamMember () {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: 'Would you like to add more Team Members?',
+            name: "addMember",
+            choices: ["Yes, add an Engineer", "Yes, add an Intern", "No, my team is complete"]
+        }
+    ])
+    .then(function (data) {
+        switch(data.addMember){
+            case "Yes, add an Engineer":
+                addEngineer();
+                break;
+            case "Yes, add an Intern":
+                addIntern();
+                break;
+            // case "No, my team is complete":
+            //     // teamBuild();
+            //     break;
+            default:
+                // teamBuild();
+        }
+    })
+ 
+  function addEngineer () {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your Engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter their ID number:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter their email:',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter their GitHub username:',
+        },
+    ]) 
+    .then(function (data) {
+        const name = data.name
+        const id = data.id
+        const email = data.email
+        const github = data.github
+        const teamMember = new Engineer(name, id, email, github)
+        finalTeam.push(teamMember)
+        addTeamMember()
+    });
+}
+const addIntern = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your Intern's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter their ID number:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter their email:',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter their School name:',
+        },
+    ])
+    .then(function (data) {
+        const name = data.name
+        const id = data.id
+        const email = data.email
+        const school = data.school
+        const teamMember = new Intern(name, id, email, school)
+        finalTeam.push(teamMember)
+        addTeamMember()
+    });
+ }
+  }
+//  function teamBuild(){
+//       fs.writeFile('index.html', generatedHTML(data), function(err){});
+//  }
+startingPrompt()
--- template.js
+++ template.js
@@ -1,4 +1,5 @@
-const generatedHTML = (responses) => 
+
+const generatedHTML = (data) => {
     `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -8,19 +9,20 @@ const generatedHTML = (responses) =>
       <title>My Team</title>
     </head>
     <body>
-    <div class="card employee-card">
+    <div class="card manager-card">
     <div class="card-header">
-        <h2 class="card-title">${responses.name}</h2>
-        <h3 class="card-title">Manager<h3>
+        <h1 class="card-title">${data.name}</h2>
+        <h2 class="card-title">Manager<h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID: ${responses.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${responses.email}}">${responses.email}</a></li>
-            <li class="list-group-item">Office number: ${responses.officeNumber}}</li>
+            <li class="list-group-item">ID: ${data.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${data.email}}">${data.email}</a></li>
+            <li class="list-group-item">Office number: ${data.officeNumber}</li>
         </ul>
     </div>
 </div>
     </body>
     </html>`;
+}
 module.exports = generatedHTML;
