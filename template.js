f0d0d99c7ecfc7cf603432d556529116dce5118d
Michael Jones <mtjones1979@yahoo.com>
2021-03-14 13:25:40 -0500

    got app to fully function

--- template.js
+++ template.js
@@ -13,17 +13,17 @@ const generateHTML = team => {
         </head>
         <body>
         <nav class="navbar navbar-dark bg-primary mb-5">
-           <span class="navbar-brand mb-3 h1 w-100 text-center text-uppercase">${manager.teamName}</span>
+           <h1 class="navbar-brand mb-3 w-100 text-center text-uppercase">${manager.teamName}</h1>
         </nav>
         <div class="container">
            <div class="row">
            <div class="col-6">
               <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header bg-success text-white"><strong>${manager.name}</strong><br /><br>Manager</h5>
+                <h5 class="card-header bg-success text-white"><strong>${manager.getName()}</strong><br /><br>Manager</h5>
                 <ul class="list-group list-group-flush">
-                   <li class="list-group-item font-weight-bold">ID: <em>${manager.id}</em></li>
-                   <li class="list-group-item font-weight-bold">Email Address: <em>${manager.email}</em></li>
-                   <li class="list-group-item font-weight-bold">Office Phone: <em>${manager.officeNumber}</em></li>
+                   <li class="list-group-item font-weight-bold">ID: <em>${manager.getId()}</em></li>
+                   <li class="list-group-item font-weight-bold">Email Address: <em>${manager.getEmail()}</em></li>
+                   <li class="list-group-item font-weight-bold">Office Phone: <em>${manager.getOfficeNumber()}</em></li>
                 </ul>
               </div>
             </div>
@@ -33,11 +33,11 @@ const generateHTML = team => {
         return `
         <div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header bg-success text-white"><strong>${engineer.name}</strong><br><br>Engineer</h5>
+                <h5 class="card-header bg-success text-white"><strong>${engineer.getName()}</strong><br><br>Engineer</h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item font-weight-bold">ID: <em>${engineer.id}</em></li>
-                    <li class="list-group-item font-weight-bold">Email Address: <em>${engineer.email}</em></li>
-                    <li class="list-group-item font-weight-bold">GitHub: <em>${engineer.github}</em></li>
+                    <li class="list-group-item font-weight-bold">ID: <em>${engineer.getId()}</em></li>
+                    <li class="list-group-item font-weight-bold">Email Address: <em>${engineer.getEmail()}</em></li>
+                    <li class="list-group-item font-weight-bold">GitHub: <em>${engineer.getGitHub()}</em></li>
                 </ul>
             </div>
         </div>
@@ -47,11 +47,11 @@ const generateHTML = team => {
         return `
         <div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header bg-success text-white"><strong>${intern.name}</strong><br><br>Intern</h5>
+                <h5 class="card-header bg-success text-white"><strong>${intern.getName()}</strong><br><br>Intern</h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item font-weight-bold">ID: <em>${intern.id}</em></li>
-                    <li class="list-group-item font-weight-bold">Email Address: <em>${intern.email}</em></li>
-                    <li class="list-group-item font-weight-bold">School: <em>${intern.school}</em></li>
+                    <li class="list-group-item font-weight-bold">ID: <em>${intern.getId()}</em></li>
+                    <li class="list-group-item font-weight-bold">Email Address: <em>${intern.getEmail()}</em></li>
+                    <li class="list-group-item font-weight-bold">School: <em>${intern.getSchool()}</em></li>
                 </ul>
             </div>
         </div>
@@ -63,13 +63,16 @@ const generateHTML = team => {
     };
     const htmlCards = [];
     htmlCards.push(team
+        .filter(employee => employee.getRole() === "Manager")
         .map(manager => generateManager(manager))
     );
    htmlCards.push(team
+        .filter(employee => employee.getRole() === "Engineer")
         .map(engineer => generateEngineer(engineer))
         .join("")
     );
     htmlCards.push(team
+        .filter(employee => employee.getRole() === "Intern")
         .map(intern => generateIntern(intern))
         .join("")
     );
